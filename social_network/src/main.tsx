f45c736fd0ce29b37e11435dfe2c6d745092e709
Esteban Marulanda <esteb_95@hotmail.com>
2026-02-03 14:45:08 -0500

    merge desde main

--- social_network/src/main.tsx
+++ social_network/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import AuthProvider from "./auth/AuthProvider";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App.tsx";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import React from "react";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
 );
